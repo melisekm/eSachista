d586f2bd74a1ab96613b6c635fa61ed09e623c42
melisekm <46221266+melisekm@users.noreply.github.com>
2021-03-13 21:27:44 +0100

    init

---
+++ Planovanie_sachovych_turnajov/src/sk/stu/fiit/main/Main.java
@@ -0,0 +1,16 @@
+package sk.stu.fiit.main;
+
+/**
+ *
+ * @author Martin Melisek
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
